c8d315d07d92104c2e114df57c4a87fedff874d8
Abdullah Mucahit <mucahit.sivasligil123@gmail.com>
2020-11-17 22:28:21 +0300

    Ikinci gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,6 +5,8 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("Ikinci Commit");
 		System.out.println("10:11 - Ikinci Gun");
+		System.out.println("10:26 - Ikinci Gun");
+		
 		
 	}
 
