a27b3e64eb89b6fe557f1c2395543b3202cbdfee
mucahit2664 <74512937+mucahit2664@users.noreply.github.com>
2020-11-17 23:23:24 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,7 +7,7 @@ public class BirinciGun {
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - Ikinci Gun");
 		System.out.println("10:35 - Ikinci Gun");
-        System.out.println("11:05 -local");
+                System.out.println("11:05 -local");
 	}
 
 }
